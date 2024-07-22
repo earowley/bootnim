5a3835187e490b1796441ddbdbdddf5f2a6a1753
Eric Rowley <earowley23@gmail.com>
2024-07-22 15:34:15 +0200

    Simple find devices process

--- lib/uefi/uefi/pci.nim
+++ lib/uefi/uefi/pci.nim
@@ -1,11 +1,10 @@
 import
-  std/options,
-  std/algorithm,
+  std/[options, algorithm, sequtils],
   ./protocols
 
-var allDevices = none(seq[PciIOProtocol])
+var allDevices = none(seq[PciIoProtocol])
 
-proc cmpDevices(a, b: PciIOProtocol): int =
+proc cmpDevices(a, b: PciIoProtocol): int =
   let ai = a.uid
   let bi = b.uid
   if ai > bi:
@@ -14,13 +13,19 @@ proc cmpDevices(a, b: PciIOProtocol): int =
     return -1
   return 0
 
-proc initAllDevices: lent seq[PciIOProtocol] =
+proc initAllDevices: lent seq[PciIoProtocol] =
   var tmp = protocols(PciIOProtocol)
   sort(tmp, cmpDevices)
   allDevices = some(tmp)
   return allDevices.get()
 
-proc fetchAllDevices*: lent seq[PciIOProtocol] =
+# Fetch protocols for all PCI devices on the system.
+proc fetchAllDevices*: lent seq[PciIoProtocol] =
   if allDevices.isSome:
     return allDevices.get()
   return initAllDevices()
+
+# Find protocols for PCI devices that have the given vendor and device IDs.
+proc findDevices*(vendor, device: uint16): seq[PciIoProtocol] =
+  let all = fetchAllDevices()
+  result = filterIt(all, it.device == device and it.vendor == vendor)
